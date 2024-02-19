dd09e24081f4cef24c010d596eecbdeb203e5985
sergiifedorishin <151446351+sergiy-fedorishin@users.noreply.github.com>
2024-02-19 15:45:12 +0200

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -3,9 +3,9 @@ function getElementWidth(content, padding, border) {
   const paddingWidth = parseFloat(padding);
   const borderWidth = parseFloat(border);
 
-  return contentWidth + paddingWidth + borderWidth;
+  return contentWidth + paddingWidth * 2 + borderWidth * 2;
 }
 
 console.log(getElementWidth('50px', '8px', '4px'));
 console.log(getElementWidth('60px', '12px', '8.5px'));
-console.log(getElementWidth('50px', '8px', '4px'));
+console.log(getElementWidth('200px', '0px', '0px'));
