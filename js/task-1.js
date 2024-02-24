8bf88babf4b46b9cb2aeb2b05f89d59f84f2d138
sergiifedorishin <151446351+sergiy-fedorishin@users.noreply.github.com>
2024-02-24 16:00:44 +0200

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -6,3 +6,37 @@ function makeTransaction(quantity, pricePerDroid) {
 console.log(makeTransaction(5, 3000));
 console.log(makeTransaction(3, 1000));
 console.log(makeTransaction(10, 500));
+
+// дз 2 завдання 1
+
+// function makeTransaction(quantity, pricePerDroid, customerCredits) {
+
+  //  Складає та повертає повідомлення про купівлю ремонтних дроїдів
+
+//   Args: quantity: Кількість замовлених дроїдів.
+//     pricePerDroid: Ціна одного дроїда.
+//     customerCredits: Сума коштів на рахунку клієнта.
+
+//   Returns:
+    //  Повідомлення про купівлю або повідомлення про недостатньо коштів.
+
+//   // Розрахунок загальної суми замовлення
+//   const total_price = quantity * pricePerDroid;
+
+  //  Перевірка наявності коштів
+//   if (customerCredits < total_price) {
+//     return "Недостатньо коштів!";
+//   } else {
+//     return `Ви замовили ${quantity} дроїдів на суму ${total_price} кредитів!`;
+//   }
+// }
+
+
+
+// const quantity = 5;
+// const pricePerDroid = 100;
+// const customerCredits = 550;
+
+// const message = makeTransaction(quantity, pricePerDroid, customerCredits);
+
+// console.log(message);
